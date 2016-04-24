c74139fc29da4b2682438d23c4f7d9ee6c00b677
Brett <bcobbs86@hotmail.com>
2016-04-23 20:50:33 -0400

    updated web

--- piApp/server/app.js
+++ piApp/server/app.js
@@ -18,49 +18,43 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 
-app.use('/', routes);
-app.use('/users', users);
-
-/// catch 404 and forwarding to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
-
-/// error handlers
-
 /**
  * Development Settings
  */
+if (app.get('env') === 'development') {
+    // This will change in production since we'll be using the dist folder
+    app.use(express.static(path.join(__dirname, '../client')));
+    // This covers serving up the index page
+    app.use(express.static(path.join(__dirname, '../client/.tmp')));
+    app.use(express.static(path.join(__dirname, '../client/app')));
 
-if ('development' == app.get('env')) {
-// This will change in production since we'll be using the dist folder
-// This covers serving up the index page
-app.use(express.static(path.join(__dirname, '../client/.tmp')));
-app.use(express.static(path.join(__dirname, '../client/app')));
-app.use(express.errorhandler());
+    // Error Handling
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
 }
 
 /**
  * Production Settings
  */
-if('production' == app.get('env')) {
-app.use(express.static(path.join(__dirname, '/dist')));
-}
+if (app.get('env') === 'production') {
 
-// development error handler
-// will print stacktrace
+    // changes it to use the optimized version for production
+    app.use(express.static(path.join(__dirname, '/dist')));
 
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
+    // production error handler
+    // no stacktraces leaked to user
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: {}
+        });
     });
-});
-
+}
 
 module.exports = app;
