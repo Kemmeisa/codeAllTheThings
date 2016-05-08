94d97a308e8a3b10b7b276b339a90fce550af81c
Brett <bcobbs86@hotmail.com>
2016-05-08 16:11:03 -0400

    Updating web interface to render 4 graphs

--- piweb/js/app.js
+++ piweb/js/app.js
@@ -10,12 +10,19 @@ $(function() {
                 "xAxisName": "Player",
                 "yAxisName": "Wickets Taken",
                 "rotatevalues": "1",
-                "theme": "zune"
+                "theme": "ocean",
+            "canvasBgAlpha": "0",
+            //Background color and alpha
+            "bgColor": "#000000",
+            "bgAlpha": "70",
+             "baseFontSize": "16",
+            "baseFontColor": "#D3D3D3",
+
             };
 
-            apiChart = new FusionCharts({
+            apiChart1 = new FusionCharts({
                 type: 'column2d',
-                renderAt: 'chart-container',
+                renderAt: 'chart-container1',
                 width: '550',
                 height: '350',
                 dataFormat: 'json',
@@ -24,7 +31,47 @@ $(function() {
                     "data": chartData
                 }
             });
-            apiChart.render();
+
+            apiChart2 = new FusionCharts({
+                type: 'column2d',
+                renderAt: 'chart-container2',
+                width: '550',
+                height: '350',
+                dataFormat: 'json',
+                dataSource: {
+                    "chart": chartProperties,
+                    "data": chartData
+                }
+            });
+
+            apiChart3 = new FusionCharts({
+                type: 'column2d',
+                renderAt: 'chart-container3',
+                width: '550',
+                height: '350',
+                dataFormat: 'json',
+                dataSource: {
+                    "chart": chartProperties,
+                    "data": chartData
+                }
+            });
+
+            apiChart4 = new FusionCharts({
+                type: 'column2d',
+                renderAt: 'chart-container4',
+                width: '550',
+                height: '350',
+                dataFormat: 'json',
+                dataSource: {
+                    "chart": chartProperties,
+                    "data": chartData
+                }
+            });
+            apiChart1.render();
+            apiChart2.render();
+            apiChart3.render();
+            apiChart4.render();
+
         }
     });
 });
